6c0fc7c0da4c5eef5b2598e40a35f1b83589ebc5
Rohit Agarwal <rohit.net@gmail.com>
2025-07-04 17:54:41 -0700

    codecrafters submit [skip ci]

--- src/torrent.rs
+++ src/torrent.rs
@@ -1,7 +1,7 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use serde_bencode::from_bytes;
 
-#[derive(Deserialize, Clone)]
+#[derive(Deserialize, Serialize, Clone, Debug)]
 pub struct Torrent {
     /// URL to a "tracker", which is a central server that keeps
     /// track of peers participating in the sharing of a torrent.
@@ -10,7 +10,7 @@ pub struct Torrent {
     pub info: TorrentInfo,
 }
 
-#[derive(Deserialize, Clone)]
+#[derive(Deserialize, Serialize, Clone, Debug)]
 pub struct TorrentInfo {
     /// Size of the file in bytes, for single-file torrents
     pub length: i64,
@@ -26,5 +26,20 @@ pub struct TorrentInfo {
 pub fn parse_torrent_file(file_path: &str) -> Torrent {
     let content = std::fs::read(file_path).expect("Cannot read torrent file.");
     from_bytes(content.as_slice()).expect("Failed to decode the torrent file.")
+}
+
+fn print_test() {
+    let info = TorrentInfo {
+        length: 5,
+        name: "tiny".to_string(),
+        piece_length: 16384,
+        pieces: serde_bytes::ByteBuf::from(vec![0u8; 2]),
+    };
+    let torrent = Torrent {
+        announce: "http://x.y/".to_string(),
+        info: info,
+    };
+    let printer = serde_bencode::to_string(&torrent).expect("Failed to convert torrent file to string.");
+    eprintln!("Torrent file content:\n{:?}", printer);
 }
 
--- tests/decode.rs
+++ tests/decode.rs
@@ -1,44 +1,66 @@
 use assert_cmd::Command;
+use tempfile::NamedTempFile;
+use std::io::Write;
+use indoc::indoc;
 
 pub const NO_ERROR: &str = "";
 pub const SUCCESS: i32 = 0;
 
 #[test] fn decode_string() {
-    run_decode("4:pear", "\"pear\"\n", NO_ERROR, SUCCESS);
-    run_decode("9:raspberry", "\"raspberry\"\n", NO_ERROR, SUCCESS);
+    run_decode("4:pear", "\"pear\"\n");
+    run_decode("9:raspberry", "\"raspberry\"\n");
+    run_decode("55:http://bittorrent-test-tracker.codecrafters.io/announce",
+               "\"http://bittorrent-test-tracker.codecrafters.io/announce\"\n");
 }
 #[test] fn decode_integer() {
-    run_decode("i1052617151e", "1052617151\n", NO_ERROR, SUCCESS);
-    run_decode("i-52e", "-52\n", NO_ERROR, SUCCESS);
+    run_decode("i1052617151e", "1052617151\n");
+    run_decode("i-52e", "-52\n");
 }
 #[test] fn decode_list() {
-    run_decode("le", "[]\n", NO_ERROR, SUCCESS);
-    run_decode("l9:pineapplei317ee", "[\"pineapple\",317]\n", NO_ERROR, SUCCESS);
-    run_decode("lli317e9:pineappleee", "[[317,\"pineapple\"]]\n", NO_ERROR, SUCCESS);
-    run_decode("lli4eei5ee", "[[4],5]\n", NO_ERROR, SUCCESS);
+    run_decode("le", "[]\n");
+    run_decode("l9:pineapplei317ee", "[\"pineapple\",317]\n");
+    run_decode("lli317e9:pineappleee", "[[317,\"pineapple\"]]\n");
+    run_decode("lli4eei5ee", "[[4],5]\n");
 }
 #[test] fn decode_dictionary() {
-    run_decode("de", "{}\n", NO_ERROR, SUCCESS);
-    run_decode("d3:foo5:apple5:helloi52ee", "{\"foo\":\"apple\",\"hello\":52}\n", NO_ERROR, SUCCESS);
+    run_decode("de", "{}\n");
+    run_decode("d3:foo5:apple5:helloi52ee", "{\"foo\":\"apple\",\"hello\":52}\n");
     run_decode("d10:inner_dictd4:key16:value14:key2i42e8:list_keyl5:item15:item2i3eeee",
-               "{\"inner_dict\":{\"key1\":\"value1\",\"key2\":42,\"list_key\":[\"item1\",\"item2\",3]}}\n", NO_ERROR, SUCCESS);
+               "{\"inner_dict\":{\"key1\":\"value1\",\"key2\":42,\"list_key\":[\"item1\",\"item2\",3]}}\n");
 }
 
-pub fn run_decode(
-    input: &str,
-    expected: &str,
-    expected_error: &str,
-    expected_code: i32)
-{
+#[test] fn decode_torrent_file() {
+    let input = b"d8:announce11:http://x.y/4:infod6:lengthi5e4:name4:tiny12:piece lengthi16384e6:pieces2:\0\0ee";
+    let expected2 = indoc! {"
+        Tracker URL: http://x.y/
+        Length: 5
+    "};
+    run_info(input, expected2);
+}
+
+pub fn run_decode(input: &str, expected: &str) {
     let mut cmd = Command::cargo_bin("codecrafters-bittorrent").expect("Binary not found");
     cmd.args(&["decode", input]);
+    let output = cmd.output().expect("Failed to run binary");
+    let stdout = String::from_utf8_lossy(&output.stdout).into_owned();
+    assert_eq!(stdout, expected);
+}
+
+pub fn run_info(input: &[u8], expected: &str) {
+    let mut temp_file = NamedTempFile::new().expect("Failed to create temp file");
+    temp_file.write_all(input).expect("Failed to write to temp file");
+
+    let mut cmd = Command::cargo_bin("codecrafters-bittorrent").expect("Binary not found");
+    cmd.args(&["info", temp_file.path().to_str().unwrap()]);
+
     let output = cmd.output().expect("Failed to run binary");
 
     let stdout = String::from_utf8_lossy(&output.stdout).into_owned();
     let stderr = String::from_utf8_lossy(&output.stderr).into_owned();
     let exit_code = output.status.code().unwrap_or(-1);
 
+    eprintln!("stdout: {}", stdout);
+    eprintln!("stderr: {}", stderr);
+    eprintln!("exit code: {}", exit_code);
     assert_eq!(stdout, expected);
-    assert_eq!(stderr, expected_error);
-    assert_eq!(exit_code, expected_code);
 }
